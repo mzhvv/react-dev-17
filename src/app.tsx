e3f90705f9db42ce2f7d2eb271f87c3536afd017
mzhvv <maximmezhevov@gmail.com>
2026-06-25 14:55:46 +0300

    .

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+// src/app.tsx
+
+export const App = () => {
+  return (
+    <div>
+      <h1>react-dev-17</h1>
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+// src/main.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { App } from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
